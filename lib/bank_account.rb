3a0f1283671377d79705cb2324d4006db79239de
mishyjari <mishyjari@outlook.com>
2020-04-02 13:34:05 -0400

    commit

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+	@@all = []
+	
+	attr_reader :name
+	attr_accessor :balance, :status
 
+	def initialize (name)
+		@name = name
+		@balance = 1000
+		@status = "open"
+		BankAccount.all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def deposit (amt)
+		self.balance += amt
+	end
+
+	def display_balance
+		"Your balance is $#{self.balance}."
+	end
+
+	def valid?
+		self.status == "open" && self.balance > 0
+	end
+
+	def close_account
+		self.status = 'closed'
+	end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
+require 'pry'
 class Transfer
-  # your code here
+	# comment
+	@@history = []
+
+	attr_accessor :sender, :receiver, :amount, :status
+
+	def initialize (sender,receiver,amount)
+		@sender = sender
+		@receiver = receiver
+		@amount = amount
+		@status = 'pending'
+		Transfer.history << self
+	end
+
+	def self.history
+		@@history
+	end
+
+	def valid?
+		#check that both accounts are valid by calling on both account's valid methods
+		sender_acct = BankAccount.all.select do | sender | 
+			sender.name == self.sender
+		end
+		receiver_acct = BankAccount.all.select do | receiver |
+			receiver.name == self.receiver
+		end
+		sender_acct.status == 'open' && receiver_acct.status == 'open'
+		binding.pry
+	end
+
+	def execute_transation
+		# Execute Successful transaction
+		# Each transaction can only happen once
+		# Reject on insufficient funds
+	end
+
+	def reverse_transfer
+		# Reverse a transfer only if its been executed
+	end
 end
